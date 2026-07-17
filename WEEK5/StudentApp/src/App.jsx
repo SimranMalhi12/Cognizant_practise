d289da675c29ef5e83a092981c92dd40b9182195
Simran <simranmalhi1612@gmail.com>
2026-07-17 11:29:19 +0530

    Week5 Reac1&2

---
+++ WEEK5/StudentApp/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import Home from './Components/Home'
+import About from './Components/About'
+import Contact from './Components/Contact'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className='Container'>
+      <Home/>
+      <About/>
+      <Contact/>
+      
+      
+       </div>
+      
+    </>
+  )
+}
+
+export default App
